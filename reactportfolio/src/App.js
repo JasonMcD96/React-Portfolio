bc5580ee54e2ba09a21991cf41588a6a40c87bd9
Jason McDonald <jasonmcd65@gmail.com>
2021-03-21 13:41:42 -0500

    added path for Portfolio

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from './Components/Navbar';
 import AboutMe from './Components/AboutMe';
+import Portfolio from './Components/Portfolio'
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Router>
-        <body>
-        <Switch>
-          <Route exact path="/" component={AboutMe}/>
-          <Route exact path="/React-Portfolio" component={AboutMe}/> {/* Just in case, popped up during testing */}
-        </Switch>
-        </body>
-      </Router>
-    </div>
+   
+      <div className="App">
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={AboutMe} />
+            <Route exact path="/React-Portfolio" component={AboutMe} /> {/* Just in case, popped up during testing */}
+            <Route exact path="/Portfolio" component={Portfolio} />
+          </Switch>
+        </Router>
+      </div>
+
   );
 }
 
