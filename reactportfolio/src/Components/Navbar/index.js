8be38712d7e1464cc1b0aff3a5657cdb3f39dbc4
Jason McDonald <jasonmcd65@gmail.com>
2021-03-21 13:42:23 -0500

    changed hrefs

--- reactportfolio/src/Components/Navbar/index.js
+++ reactportfolio/src/Components/Navbar/index.js
@@ -5,7 +5,7 @@ function Navbar() {
     return(
         <div>
             <nav id="myNavbar" className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <a className="navbar-brand" href="index.html">
+            <a className="navbar-brand" href="/">
                 <p id="myName">Jason McDonald</p>
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -15,13 +15,13 @@ function Navbar() {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="index.html">About Me<span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/">About Me<span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="portfolio.html">Portfolio</a>
+                        <a className="nav-link" href="Portfolio">Portfolio</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="contact.html">Contact</a>
+                        <a className="nav-link" href="Contact">Contact</a>
                     </li>
 
                 </ul>
