fc70aa437e11718e15116124ff6f9ab5b9d2bbe8
Jason McDonald <jasonmcd65@gmail.com>
2021-03-21 13:46:30 -0500

    formatting

--- reactportfolio/src/Components/Portfolio/index.js
+++ reactportfolio/src/Components/Portfolio/index.js
@@ -4,31 +4,32 @@ import projects from "./projects.json"
 
 function Portfolio() {
 
-    return (<div>
-        <main className="middlePage">
-            <div id="portfolio" className="container">
-                <div className="row">
-                    <div className="col">
-                        <h1>Portfolio</h1>
+    return (
+        <div>
+            <main className="middlePage">
+                <div id="portfolio" className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1>Portfolio</h1>
+                        </div>
+                    </div>
+                    <hr />
+
+                    <div className="row align-items-center text-center">
+                        {projects.map(project => (
+
+                            <PortfolioItem
+                                key={Math.random()}
+                                title={project.title}
+                                github={project.github}
+                                deploy={project.deploy}
+                                img={project.img}
+                            />
+                        ))}
                     </div>
                 </div>
-                <hr />
-
-                <div className="row align-items-center text-center">
-                    {projects.map(project => (
-
-                        <PortfolioItem 
-                        key={Math.random()}
-                        title={project.title}
-                        github={project.github}
-                        deploy={project.deploy}
-                        img={project.img}
-                        />
-                    ))}
-                </div>
-            </div>
-        </main>
-    </div>)
+            </main>
+        </div>)
 }
 
 export default Portfolio
