e1794a7de06a64e26d1c28a23dd66d63ab3a92ac
Jason McDonald <jasonmcd65@gmail.com>
2021-03-21 12:36:30 -0500

    path for about me

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from './Components/Navbar';
+import AboutMe from './Components/AboutMe';
 
 
 function App() {
@@ -9,9 +10,12 @@ function App() {
     <div className="App">
       <Navbar />
       <Router>
+        <body>
         <Switch>
-          {/* <Route exact path="/" component={} */}
+          <Route exact path="/" component={AboutMe}/>
+          <Route exact path="/React-Portfolio" component={AboutMe}/> {/* Just in case, popped up during testing */}
         </Switch>
+        </body>
       </Router>
     </div>
   );
